157267693d563bef110183e26b269d013cd34a9f
Li Xiang <c0dezlee@gmail.com>
2019-07-20 18:42:25 +0800

    bug fix

--- Bosch/message/views.py
+++ Bosch/message/views.py
@@ -15,4 +15,4 @@ class MessageViewSet(viewsets.ViewSet):
 		if request.data:
 			print(request.data)
 		
-		return Response({ 'data':'1', 'code':'200', 'message':"xxxxx", 'uploadeData': request.data})
+		return Response({ 'data':1, 'code':200, 'message':"xxxxx", 'uploadeData': request.data})
