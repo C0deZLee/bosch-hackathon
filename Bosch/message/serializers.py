b9a402c8de1953250ee3d30d065a384db29c3fed
Li Xiang <c0dezlee@gmail.com>
2019-07-20 22:14:32 +0800

    bug fix

--- Bosch/message/serializers.py
+++ Bosch/message/serializers.py
@@ -5,6 +5,6 @@ from .models import Message
 class MessageSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Message
-		fields = ('id', 'x', 'y', 'z', 'time', 'created',)
+		fields = ('id', 'x', 'y', 'z', 'time', 'label', 'created',)
 		read_only_fields = ('created',)
 
--- Bosch/message/views.py
+++ Bosch/message/views.py
@@ -4,7 +4,7 @@ from rest_framework.parsers import FormParser, JSONParser, MultiPartParser, File
 
 from .models import Message
 from .serializers import MessageSerializer
-from .web_itfc import predict, save_data, retrain
+# from .web_itfc import predict, save_data, retrain
 
 class MessageViewSet(viewsets.ViewSet):
 	parser_classes = (JSONParser,)
@@ -17,22 +17,27 @@ class MessageViewSet(viewsets.ViewSet):
 		time = request.data["time"]
 
 		# save
-		label = predict(x, y, z, time)
-		msg = Message(x = x, y = y, z = z, time = time, label = label)
+		# label = predict(x, y, z, time)
+		msg = Message(x = x, y = y, z = z, time = time, label = 'label')
 
 		msg.save()
 
-		return Response({ 'data': label, 'code': 200, 'id': msg.id})
+		return Response({ 'data': 'label', 'code': 200, 'id': msg.id})
 
 	def reinforce(self, request):
 		id = request.data["id"]
 		label = request.data["label"]
-
+		
 		msg = Message.objects.get(id=id)
-		msg.objects.update(label = str(label))
+		msg.label = label
+		msg.save()
+
+		serializer = MessageSerializer(msg)
+
+		print(msg.x, msg.y, msg.z, msg.time, label)
 		# call reinforce function
-		save_data(msg["x"], msg["y"], msg["z"], msg["time"], label)
-		return Response(status=status.HTTP_200_OK)
+		# save_data(msg["x"], msg["y"], msg["z"], msg["time"], label)
+		return Response(serializer.data, status=status.HTTP_200_OK)
 
 	def all(self, request):
 		msgs = Message.objects.all()
